cfb22cc94fd9e899e405b053cdbcb80707ea0c38
CabbageOni <pos02232@gmail.com>
2020-07-08 18:45:35 +0900

    finished implementing vec2 variations.

--- graphics_math.hpp
+++ graphics_math.hpp
@@ -21,19 +21,52 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+#include <ostream>
 
 namespace graphics_math
 {
+  template<typename T>
   union vec2
   {
-    float data[2] = {0, 0};
-    struct { float x, y; };
+  public:
+    T data[2] = {0, 0};
+    struct { T x, y; };
+    struct { T width, height; };
+
+    inline vec2(const T& uniform) : x(uniform), y(uniform) {}
+    inline vec2(const T& x, const T& y) : x(x), y(y) {}
+    inline vec2(const vec2& rhs) : x(rhs.x), y(rhs.y) {}
+
+    inline T& operator[](const size_t index) noexcept(false) { if (index > 1) throw "index out of bound!"; return data[index]; }
+
+    inline const vec2 operator+(const vec2& rhs) const { return { x + rhs.x, y + rhs.y }; }
+    inline const vec2 operator-(const vec2& rhs) const { return { x - rhs.x, y - rhs.y }; }
+    inline const vec2 operator*(const    T& rhs) const { return { x * rhs, y * rhs }; }
+    inline const vec2 operator*(const vec2& rhs) const { return { x * rhs.x, y * rhs.y }; }
+    inline const vec2 operator/(const    T& rhs) const { return { x / rhs, y / rhs }; }
+
+    inline vec2& operator+=(const vec2& rhs) { x += rhs.x; y += rhs.y; return *this; }
+    inline vec2& operator-=(const vec2& rhs) { x -= rhs.x; y -= rhs.y; return *this; }
+    inline vec2& operator*=(const vec2& rhs) { x *= rhs.x; y *= rhs.y; return *this; }
+    inline vec2& operator*=(const    T& rhs) { x *= rhs; y *= rhs; return *this; }
+    inline vec2& operator/=(const vec2& rhs) { x /= rhs.x; y /= rhs.y; return *this; }
+
+    inline const T    dot(const vec2& rhs) const { return x * rhs.x + y * rhs.y; }
+    inline const T    length_squared() const     { return dot(*this); }
+    inline const T    length() const             { return sqrt(length_squared()); }
+    inline vec2&      normalize()                { return *this /= length(); }
+    inline const vec2 normalized() const         { return vec2(*this) /= length(); }
+
+    template <template<typename> class U, typename V>
+    inline explicit operator U<V>() const { return { (V)x, (V)y }; }
   };
 
-  union ivec2
+  template<typename T>
+  inline std::ostream& operator<<(std::ostream& os, const vec2<T>& rhs)
   {
-    int data[2] = {0, 0};
-    struct { int x, y; };
-    struct { int width, height; };
-  };
+    return os << "(" << rhs.x << ", " << rhs.y << ")";
+  }
+
+  using fvec2 = vec2<float>;
+  using ivec2 = vec2<int>;
 }
